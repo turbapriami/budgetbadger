4ddd8e35cd04dc5d6f5760fe0f4317d0180ee11a
GustafN <gustaf.brostedt@gmail.com>
2018-01-23 19:39:34 -0500

    add entry point

---
+++ client/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+  <Provider>
+    <App/>
+  </Provider>,
+  document.getElementById('app')
+);
