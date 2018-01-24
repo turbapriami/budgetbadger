a3cb886417ffc53d6bbbb859bcd05d1a144632d5
GustafN <gustaf.brostedt@gmail.com>
2018-01-23 20:02:50 -0500

    setup redux provider

--- client/index.js
+++ client/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import reducers from './redux/reducers/index.js'
+import App from './components/pages/App.jsx';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 const store = createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-ReactDOM.render(
-  <Provider>
-    <App/>
-  </Provider>,
-  document.getElementById('app')
-);
+document.addEventListener('DOMContentLoaded', () => {
+  return (
+    ReactDOM.render(
+      <Provider store={ store }>
+        <App/>
+      </Provider>,
+      document.getElementById('app')
+    )
+  )
+})
