e1a0c797c0fd3f815353c20ea50f4dcea76a1fd8
GustafN <gustaf.brostedt@gmail.com>
2018-01-23 19:13:49 -0500

    setup express server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const config = require('./config/index.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const morgan = require('morgan');
+const passport = require('passport');
+const port = config.PORT;
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+
+app.listen(port, (err) => {
+  console.log('Listening on port: ' + port);
+});
+
+module.exports = app;
