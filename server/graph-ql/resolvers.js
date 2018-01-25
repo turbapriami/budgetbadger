88cc9e347dface2733c5ba3865daedc266672526
GustafN <gustaf.brostedt@gmail.com>
2018-01-25 11:13:03 -0500

    add schema and resolvers for login/signup

---
+++ server/graph-ql/resolvers.js
@@ -0,0 +1,57 @@
+const jwt = require('jsonwebtoken');
+const _ = require('lodash')
+
+module.exports = {
+  User: {
+    transactions: ({ id }, args, { knex }) => 
+      knex('transactions').where({
+        userid: id
+      })
+  },
+
+  Transaction: {
+    user: ({ userid }, args, { knex }) => 
+      knex('users').where({
+        id: userid
+      })
+  },
+
+  Query: {
+    getUser: (parent, { email }, { knex, user }) => {
+      if (user) {
+        knex('users').where({
+          email
+        })
+      } else {
+        throw new Error('Not authenticated')
+      }
+    },
+
+    getTransactions: (parent, { userid }, { knex }) => 
+      knex('transactions').where({
+        userid
+      }),
+
+    },
+  Mutation: {
+    createUser: async (parent, args, { dbUser }) => await new dbUser(args).save(),
+    loginUser: async (parent, { email, password }, { dbUser, APP_SECRET }) => {
+      const newUser = await new dbUser({ email }).fetch();
+      if (!newUser) {
+        throw new Error('Unable to match the prodided credentials');
+      }
+
+      const match = await newUser.comparePassword(password);
+      if (!match) {
+        throw new Error('Unable to match the provided credentials');
+      }
+
+      const token = jwt.sign({ user: _.pick(newUser.attributes, ['id', 'email'])}, APP_SECRET, {
+        expiresIn: 360*60
+      })
+      return token
+    },
+    createTransaction: (parent, args, { knex }) => knex('transactions').insert(args),
+    deleteUser: (parent, args, { knex }) => knex('users').where(args).fetch().del(),
+  }
+}
--- server/graph-ql/schema.js
+++ server/graph-ql/schema.js
@@ -1,164 +1,28 @@
-const knex = require('../database/index.js').knex;
-const dbUser = require('../database/models/user.js');
-const {
-  GraphQLObjectType,
-  GraphQLInt,
-  GraphQLList,
-  GraphQLString,
-  GraphQLSchema,
-  GraphQLNonNull
-} = require('graphql');
-
-
-const User = new GraphQLObjectType({
-  name: 'User',
-  description: 'User object',
-  fields: () => {
-    return {
-      id: {
-        type: GraphQLInt,
-        resolve(user) {
-          return user.id;
-        }
-      },
-      username: {
-        type: GraphQLString,
-        resolve(user) {
-          return user.username
-        }
-      },
-      email: {
-        type: GraphQLString,
-        resolve(user) {
-          return user.email
-        }
-      },
-      transactions: {
-        type: new GraphQLList(Transaction),
-        resolve(user) {
-          return knex('transactions').where({userid: user.id})
-        }
-      }
-    }
-  }
-});
-
-const Transaction = new GraphQLObjectType({
-  name: 'Transaction',
-  descirption: 'This is a transaction',
-  fields: () => {
-    return {
-      id: {
-        type: GraphQLInt,
-        resolve(transaction) {
-          return transaction.id;
-        }
-      },
-      amount: {
-        type: GraphQLString,
-        resolve(transaction) {
-          return transaction.amount;
-        }
-      },
-      category: {
-        type: GraphQLString,
-        resolve(transaction) {
-          return transaction.category;
-        }
-      },
-      userid: {
-        type: GraphQLInt,
-        resolve(transaction) {
-          return transaction.userid;
-        }
-      },
-      user: {
-        type: new GraphQLList(User),
-        resolve(transaction) {
-          return knex('users').where({id: transaction.userid})
-        }
-      }
-    }
-  }
-})
-
-const Query = new GraphQLObjectType({
-  name: 'Query',
-  descirption: 'This is a root query',
-  fields: () => {
-    return {
-      users: {
-        type: new GraphQLList(User),
-        args: {
-          id: {
-            type: GraphQLInt
-          },
-          email: {
-            type: GraphQLString
-          },
-          username: {
-            type: GraphQLString
-          },
-        },
-        resolve(root, args) {
-          return knex('users').where(args)
-        }
-      },
-      transactions: {
-        type: new GraphQLList(Transaction),
-        args: {
-          id: {
-            type: GraphQLInt
-          },
-          amount: {
-            type: GraphQLString
-          },
-          category: {
-            type: GraphQLString
-          },
-          userId: {
-            type: GraphQLInt
-          }
-        },
-        resolve(root, args) {
-          return knex('transactions').where(args)
-        }
-      }
-    }
-  }
-});
-
-const Mutation = new GraphQLObjectType({
-  name: 'Mutation',
-  description: 'Mutation',
-  fields: () => {
-    return {
-      newUser: {
-        type: User,
-        args: {
-          username: {
-            type: new GraphQLNonNull(GraphQLString)
-          },
-          password: {
-            type: new GraphQLNonNull(GraphQLString)
-          }
-        },
-        resolve(root, args) {
-          return new dbUser({
-            username: args.username,
-            password: args.password
-          }).save()
-        }
-      }
-    }
+module.exports = `
+  type User {
+    id: Int!
+    email: String!
+    password: String!
+    transactions: [Transaction!]
   }
 
-})
+  type Transaction {
+    id: Int!
+    userid: Int!
+    amount: Int!
+    category: String!
+    user: User!
+  }
 
-const Schema = new GraphQLSchema({
-  query: Query,
-  mutation: Mutation
-});
+  type Query {
+    getUser(email: String!): [User!]
+    getTransactions(userid: Int!): [Transaction!]
+  }
 
-module.exports = Schema;
-
+  type Mutation {
+    createTransaction(userid: Int!, amount: Int!, category: String!): Transaction
+    createUser(email: String!, password: String!): User
+    deleteUser(email: String!): Int!
+    loginUser(email: String!, password: String!): String!
+  }
+`
