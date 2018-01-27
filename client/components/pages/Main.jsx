182611d8c7f778791adc6c737995fd15692b8f5c
Your Name <you@example.com>
2018-01-27 15:08:12 -0500

    selecting profile deselects current tab

--- client/components/pages/Main.jsx
+++ client/components/pages/Main.jsx
@@ -20,9 +20,21 @@ class Main extends React.Component {
     }
   }
   componentWillMount() {
+    console.log('mounting')
     this.setState({
       activeIndex: this.indexChooser[this.props.location.pathname]
     })
+  }
+  componentWillReceiveProps() {
+    setTimeout(()=> {
+      if (this.props.location.pathname === '/profile') {
+        this.setState({
+          activeIndex: 5
+        }, ()=> {
+          console.log(this.props.location.pathname)
+        })
+      }
+    }, 1)
   }
   render() {
     return (
