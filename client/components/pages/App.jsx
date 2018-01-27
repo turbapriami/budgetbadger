292c99c33b192c35492582c95b17b4ee1f3318e7
loganmcbride <32717846+loganmcbride@users.noreply.github.com>
2018-01-27 18:00:45 -0500

    update

--- client/components/pages/App.jsx
+++ client/components/pages/App.jsx
@@ -4,11 +4,7 @@ import LoansContainer from '../containers/LoansContainer.jsx';
 export default class extends Component {
   render() {
     return (
-      <div>
         <h1> hello world </h1>
-        <p></p>
-        <LoansContainer />
-      </div>
     )
   }
 } 
