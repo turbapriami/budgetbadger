d9b21514c0d19933c0f550e9c35132bd7863b61d
GustafN <gustaf.brostedt@gmail.com>
2018-01-29 10:10:44 -0500

    add transactions page

--- client/components/pages/transactions/TransactionList.jsx
+++ client/components/pages/transactions/TransactionList.jsx
@@ -3,20 +3,19 @@ import SideBar from 'grommet/components/SideBar';
 import Table from 'grommet/components/Table';
 import TableHeader from 'grommet/components/TableHeader';
 import Transaction from './Transaction.jsx';
+import { Box } from 'grommet'
 
 
 
 const TransactionList = (props) => (
-  <div>
       <Table>
         <TableHeader
-          labels={['Date', 'Category', 'Description', 'Amount']}
+          labels={['Date', 'Type', 'Category','Description', 'Amount']}
         />
         {props.transactions ? props.transactions.map((transaction, idx) => {
-          return <Transaction {...transaction} kew={idx}/>
+          return <Transaction {...transaction} key={idx}/>
         }): null}
       </Table>
-  </div>
 )
 
 
