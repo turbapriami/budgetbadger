81c790f9692b9c579d22f25be73dc28437961fcd
afriedman1991@gmail.com <afriedman1991@gmail.com>
2018-01-29 14:24:38 -0500

    Fixing merge conflict

--- client/components/pages/Splash.jsx
+++ client/components/pages/Splash.jsx
@@ -7,28 +7,7 @@ export default class extends Component {
     return (
       <App centered={false}>
         <Article>
-          <div className="grommetux-box grommetux-box--direction-column grommetux-box--responsive grommetux-box--pad-none grommetux-box--flex-off">
-            <Header className="grommetux-box grommetux-box--direction-row grommetux-box--justify-center grommetux-box--align-center grommetux-box--pad-horizontal-none grommetux-box--pad-vertical-none grommetux-box--pad-between-small grommetux-background-color-index-neutral-4 grommetux-background-color-index--dark grommetux-header" fixed={false}
-              float={false}
-              size='medium'
-              splash={false}
-              style={{ background: "#FFF" }} >
-              <div className="grommetux-box grommetux-box--direction-row grommetux-box--justify-start grommetux-box--align-center grommetux-box--flex-grow grommetux-box--pad-horizontal-medium grommetux-box--width-max-xxlarge grommetux-box--width-max">
-                <img width="80" height="80" src="https://lh3.googleusercontent.com/4e2brTXTKqTVQ4GN5uFdBrPWfyxeP6_d-5wP2gXLhBHuyQyDjyT2qQC7qCUpTgjhnnjHb3D6G8EVRbgcBfrCOxUhe9GFqni-jfgJhsWCLW0AGAJKcb1F04ZvpwiPNkBRMDS_8ymcjbSLMlZMZgD3l14r616OKDQKkp7qf3aD9rvEY_wMN6OkFPawwP8DuFwKZA5THYVDkRtVcSYXdwcxkMMJD9J6J5jx5wFM-FRr0EJrxW5r3DJqpXCVmbre1TWlQg7NXklgs6aKipIkmXRJZ7BwKzJWwS91TWEYhmXDgieKULMgzik0xV7yY070GNCquebFPUcfgidIPdJhSeRXclwTsoRKjM0hhd0874BJ7dLHUnexti8Vc4ce1SJL1q1EX-KsGKtkKRCX2GvHTTphfPT29F4UxCGMcnPHIRLXSdwbmJMn0XXuDeLf81UtR2_KGaqmO-ow0WDsYA4gAbxIY3BFbJg-2_eJV8YtG3RVFrWCAE_sPUQ3eEnUIYww7049TRrsYAt7uqjsrXIO7bd_y8c4wheXoXvPm6_DnG3GXk7Se7MVXohO_g8A1sbiftZmpyQXdxHjqKn2_huxBxkdLbV4S4kFRU651mfw5e4=s600-no"/>
-                <div className="grommetux-box grommetux-box--direction-column grommetux-box--responsive grommetux-box--pad-small"></div>
-                <nav className="grommetux-box grommetux-box--direction-row grommetux-box--responsive grommetux-box--flex-grow grommetux-box--pad-none grommetux-menu grommetux-menu--row grommetux-menu--inline">
-                  <a href="#" style={{ color: "#000" }} class="grommetux-anchor">About</a>
-                  <a href="#" style={{ color: "#000" }} class="grommetux-anchor">Login</a>
-                  <a href="#" style={{ color: "#000" }} class="grommetux-anchor">Sign Up</a>
-                </nav>
-                <Box flex={true}
-                  justify='end'
-                  direction='row'
-                  responsive={false}>
-                </Box>
-              </div>
-            </Header>
-          </div>
+
 
           <Box full="horizontal">
             <div className="grommetux-box grommetux-box--direction-column grommetux-box--responsive grommetux-box--pad-none grommetux-background-color-index-light-1 grommetux-background-color-index--light grommetux-hero grommetux-hero--large grommetux-hero--bg-center">
