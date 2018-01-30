d9f0e7604a183a863303c9a77aa6ad12de97d87f
GustafN <gustaf.brostedt@gmail.com>
2018-01-30 14:08:23 -0500

    refactor transaction query

--- client/components/pages/transactions/PieChart.jsx
+++ client/components/pages/transactions/PieChart.jsx
@@ -1,2 +1,20 @@
-import React from 'react';
-import chart from 'c3';
+import React, { Compoennt } from 'react';
+import forEach from 'lodash';
+import c3 from 'c3'
+
+const PieChart = ({ breakdown }) => {
+  if (breakdown.length) {
+    let chart = c3.generate({
+      bindto: '.chart',
+      data: {
+        columns: breakdown,
+        type: 'donut'
+      }
+    });
+  }
+  return (
+    <div className='chart'/>
+  )
+}
+
+export default PieChart;
