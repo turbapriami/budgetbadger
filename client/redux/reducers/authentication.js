174e73aa175e69b7989840d5bc00b49a4a0371c2
Jimmy Kang <jimmykang@Jimmys-MacBook-Pro.local>
2018-01-30 14:04:39 -0500

    fixing merge conflicts

---
+++ client/redux/reducers/authentication.js
@@ -0,0 +1,17 @@
+import { AUTH_USER,  
+         UNAUTH_USER,
+         AUTH_ERROR } from '../actions/types.js';
+
+const INITIAL_STATE = { errors: '', message: '', content: '', authenticated: false };
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case AUTH_USER:
+      return { ...state, error: '', message: '', authenticated: true };
+    case UNAUTH_USER:
+      return { ...state, error: '', message: '', authenticated: false };
+    case AUTH_ERROR:
+      return { ...state, error: action.payload };    
+  }
+  return state;
+}
