a3f924a2ffcd5a8daa50cf6349f39a85f8b45495
afriedman1991@gmail.com <afriedman1991@gmail.com>
2018-01-29 20:01:26 -0500

    Started to query for user auth

--- server/index.js
+++ server/index.js
@@ -45,11 +45,11 @@ const logger = (req, res, next) => {
   console.log(req.body)
   next();
 }
-app.use(express.static(path.join(__dirname, '../public/splash')))
+// app.use(express.static(path.join(__dirname, '../public/splash')))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../public/splash', 'index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../public/splash', 'index.html'))
+// })
 
 // app.use(getToken); // => uncomment to enable authentication
 
