f44000cbd5f0d183ea7291f03845260bd7dc30dd
Your Name <andrewbl@icloud.com>
2018-02-02 19:01:06 -0500

    changes

--- client/components/containers/TransactionContainer.jsx
+++ client/components/containers/TransactionContainer.jsx
@@ -159,8 +159,7 @@ class TransactionContainer extends Component {
     variables.user_id = 1;
     variables.account_id = this.props.data.getAccounts[variables.account.slice(0, variables.account.indexOf('.'))].id
     variables.amount = Number(variables.amount);
-    variables.type = 'd'
-    // const transaction = await this.props.createNewTransaction({variables});
+    const transaction = await this.props.mutate({variables});
     const { transactions } = this.state
     transactions.unshift(variables);
     this.setState({
@@ -209,9 +208,7 @@ class TransactionContainer extends Component {
         </div>
       )
     } else {
-      return (
-        <Spinner />
-      )
+      return <Spinner />
     }
   }
 }
