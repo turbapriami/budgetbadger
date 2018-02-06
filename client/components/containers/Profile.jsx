ef5999ca665449853946010a1ed0cb28cd22fabb
afriedman1991@gmail.com <afriedman1991@gmail.com>
2018-02-05 19:26:48 -0500

    Completed update user feature

--- client/components/containers/Profile.jsx
+++ client/components/containers/Profile.jsx
@@ -6,7 +6,6 @@ import ProfileCard from './ProfileCard.jsx';
 import ProfileEdit from './ProfileEdit.jsx';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import { App, Header, Section, Footer, Article, Title, Box, Paragraph, Menu, Anchor, Card, TextInput } from 'grommet';
-<<<<<<< ec31cdb55239928646800270fc98aa420cfc3ca1
 
 const Get_User = gql`
   query GETUSER($id: Int!) {
@@ -31,33 +30,6 @@ const currentUser = graphql(Get_User, {
     name: 'currentUser',
   })
 })
-=======
->>>>>>> Working on edit profile
-
-const Get_User = gql`
-  query GETUSER($id: Int!) {
-    getUser(id: $id) {
-      first_name
-      last_name
-      email
-      street
-      state
-      city
-      zip_code
-      phone
-    }
-  }
-`
-
-const currentUser = graphql(Get_User, {
-  options: props => ({
-    variables: {
-      id: 27
-    },
-    name: 'currentUser',
-  })
-})
-
 
 class Profile extends Component {
   constructor(props) {
@@ -74,6 +46,7 @@ class Profile extends Component {
       edit: false
     }
     this.editing = this.editing.bind(this);
+    this.handleChanges = this.handleChanges.bind(this);
     this.updateFirstName = this.updateFirstName.bind(this);
     this.updateLastName = this.updateLastName.bind(this);
     this.updateStreet = this.updateStreet.bind(this);
@@ -84,12 +57,12 @@ class Profile extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    console.log("NEXT PROPS!!!!!!!!", nextProps.data);
     if (nextProps.data.getUser) {
       this.setState({
         first_name: nextProps.data.getUser[0].first_name,
         last_name: nextProps.data.getUser[0].last_name,
         street: nextProps.data.getUser[0].street,
-        city: nextProps.data.getUser[0].city,
         state: nextProps.data.getUser[0].state,
         zip_code: nextProps.data.getUser[0].zip_code,
         phone: nextProps.data.getUser[0].phone,
@@ -100,51 +73,60 @@ class Profile extends Component {
 
   editing() {
     this.setState({
-<<<<<<< ec31cdb55239928646800270fc98aa420cfc3ca1
-=======
       edit: !this.state.edit
     })
   }
 
+  handleChanges() {
+    console.log("MUTATION QUERY");
+    this.editing();
+  }
+
   updateFirstName(e) {
+    console.log("FIRST NAME",e.target.value);
     this.setState({
       first_name: e.target.value
     })
   }
 
   updateLastName(e) {
+    console.log("LAST NAME", e.target.value);
     this.setState({
       last_name: e.target.value
     })
   }
 
   updateStreet(e) {
+    console.log("STREET", e.target.value);
     this.setState({
       street: e.target.value
     })
   }
 
   updateState(e) {
+    console.log("STATE", e.target.value);
     this.setState({
       state: e.target.value
     })
   }
 
   updateZipCode(e) {
+    console.log("ZIP CODE", e.target.value);
     this.setState({
       zip_code: e.target.value
     })
   }
 
   updatePhone(e) {
+    console.log("PHONE", e.target.value);
     this.setState({
       phone: e.target.value
     })
   }
 
   updateEmail(e) {
+    console.log("EMAIL", e.target.value);
     this.setState({
->>>>>>> Working on edit profile
       email: e.target.value
     })
   }
