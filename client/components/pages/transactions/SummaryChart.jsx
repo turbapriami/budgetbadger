6344017420347c2712d0fa17107b93a7da58c685
GustafN <gustaf.brostedt@gmail.com>
2018-02-06 19:57:32 -0500

    dynamically render daily transactions on click

--- client/components/pages/transactions/SummaryChart.jsx
+++ client/components/pages/transactions/SummaryChart.jsx
@@ -4,21 +4,26 @@ import { Select } from 'grommet';
 import Layer from 'grommet/components/Layer';
 import { identity, map } from 'lodash';
 import { Line, Bar } from 'react-chartjs-2'
-
-
 import moment from 'moment'
 
 class SummaryChart extends React.Component {
   constructor() {
     super();
-    this.state = {chartData:{}, begDate: null, endDate:null}
+    this.state = {
+      chartData: {}, 
+      begDate: null, 
+      endDate:null, 
+      chartToggle: true
+    }
     this.getTransactionsFromTo = this.getTransactionsFromTo.bind(this);
     this.assignToDate = this.assignToDate.bind(this);
     this.assignToMonth = this.assignToMonth.bind(this);
     this.generateMonthlyChart = this.generateMonthlyChart.bind(this);
     this.renderChart = this.renderChart.bind(this);
-    this.handleMonthlyClick = this.handleMonthlyClick.bind(this);
+    this.handleChartClick = this.handleChartClick.bind(this);
   }
+
+  // Filters transactions by date range
 
   getTransactionsFromTo(begDate = '1990-10-10', endDate = new Date()) {
     let beg = moment(begDate);
@@ -28,6 +33,8 @@ class SummaryChart extends React.Component {
       return (curr >= beg && curr <= end)
     });
   }
+
+  // Groups annual transactions by date
 
   assignToDate (transactions) {
     let dates = {};
@@ -40,6 +47,8 @@ class SummaryChart extends React.Component {
     return dates;
   }
 
+  // Groups annual transactions by month
+
   assignToMonth (transactions) {
     let months = {};
     transactions.forEach(transaction => {
@@ -51,17 +60,32 @@ class SummaryChart extends React.Component {
     return months;
   }
 
+  // Takes daily transactions and calculates total spend for the day for each date
+  // then sets state to rerender the chart
+
   generateDailyChart (transactions, month, filter = identity) {
-    let dates = this.assignToDate(transactions);
-    let chartData = {};
-    chartData.labels = Object.keys(dates);
-    chartData.data = map(dates, day => {
-      return day.reduce((a, b) => {
+    const monthlyTransactions = this.assignToMonth(transactions)[month];
+    const dailyTransactions = this.assignToDate(monthlyTransactions)
+    const days = [...Object.keys(dailyTransactions)];
+    const amounts = days.map(day => {
+      return dailyTransactions[day].reduce((a, b) => {
         return filter(b) ? a += Math.abs(b.amount):  a;
       }, 0)
     })
+    const chartData = {
+      labels: days, 
+      datasets:[
+      {
+        label: 'Monthly Amounts',
+        data: [...amounts]
+      }]
+    }
     return chartData;
   }
+
+
+  // Takes monthly transactions and calculates total spend for the month
+  // then sets state to rerender the chart
 
   generateMonthlyChart (transactions, filter = identity) {
     const months = this.assignToMonth(transactions);
@@ -91,15 +115,29 @@ class SummaryChart extends React.Component {
     })
   }
 
-  handleMonthlyClick(element) {
 
+  // chartToggle checks whether or not a month has been selected
+  // if true, the chart will render daily transactions for the selected month
+  // else it will reset to all transactions for the year
+
+  handleChartClick(element) {
+    let chartData;
+    if (this.state.chartToggle) {
+      const month = this.state.chartData.labels[element[0]._index];
+      chartData = this.generateDailyChart(this.props.transactions, month);
+    } else {
+      chartData = this.generateMonthlyChart(this.props.transactions);
+    }
+    this.setState({
+      chartData,
+      chartToggle: !this.state.chartToggle
+    })
   }
 
   componentDidMount() {
     let { name } = this.props.summaryTransaction;
     let range = this.getTransactionsFromTo();
     let chartData = this.generateMonthlyChart(range, (transaction) => transaction.name === name);
-    console.log(chartData)
     this.setState({chartData})
   }
 
@@ -143,7 +181,7 @@ class SummaryChart extends React.Component {
     }
     return (
       <div>
-        <Line data={this.state.chartData} options={options} width="600" height="500" getElementAtEvent={(element) => this.handleMonthlyClick(element)}/>
+        <Line data={this.state.chartData} options={options} width="600" height="500" getElementAtEvent={(element) => this.handleChartClick(element)}/>
         <Select 
           placeholder="Select a category"
           options={this.props.categories.map(a => a[0])}
