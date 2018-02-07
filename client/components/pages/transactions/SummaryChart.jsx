b274c5e25301399a3ab35d152708d17d7223dc7e
GustafN <gustaf.brostedt@gmail.com>
2018-02-06 19:23:56 -0500

    refactor chartjs

--- client/components/pages/transactions/SummaryChart.jsx
+++ client/components/pages/transactions/SummaryChart.jsx
@@ -3,20 +3,21 @@ import ReactDOM from 'react-dom';
 import { Select } from 'grommet';
 import Layer from 'grommet/components/Layer';
 import { identity, map } from 'lodash';
-import c3 from 'c3'
-import 'c3/c3.css'
+import { Line, Bar } from 'react-chartjs-2'
+
 
 import moment from 'moment'
 
 class SummaryChart extends React.Component {
   constructor() {
     super();
-    this.state = {chartData:[], chart: {}, begDate: null, endDate:null}
+    this.state = {chartData:{}, begDate: null, endDate:null}
     this.getTransactionsFromTo = this.getTransactionsFromTo.bind(this);
     this.assignToDate = this.assignToDate.bind(this);
-    this.generateChartInput = this.generateChartInput.bind(this);
-    this.lineChart = this.lineChart.bind(this);
+    this.assignToMonth = this.assignToMonth.bind(this);
+    this.generateMonthlyChart = this.generateMonthlyChart.bind(this);
     this.renderChart = this.renderChart.bind(this);
+    this.handleMonthlyClick = this.handleMonthlyClick.bind(this);
   }
 
   getTransactionsFromTo(begDate = '1990-10-10', endDate = new Date()) {
@@ -31,96 +32,126 @@ class SummaryChart extends React.Component {
   assignToDate (transactions) {
     let dates = {};
     transactions.forEach(transaction => {
-      dates[transaction.date] ?
-      dates[transaction.date].push(transaction) :
-      dates[transaction.date] = [transaction];
+      let date = transaction.date;
+      dates[date] ?
+      dates[date].push(transaction) :
+      dates[date] = [transaction];
     })
     return dates;
   }
 
-  generateChartInput (transactions, filter = identity) {
+  assignToMonth (transactions) {
+    let months = {};
+    transactions.forEach(transaction => {
+      let month = moment(transaction.date).format('MMMM')
+      months[month] ?
+      months[month].push(transaction) :
+      months[month] = [transaction];
+    })
+    return months;
+  }
+
+  generateDailyChart (transactions, month, filter = identity) {
     let dates = this.assignToDate(transactions);
-    let input = [];
-    input[1] = Object.keys(dates);
-    input[0] = map(dates, day => {
+    let chartData = {};
+    chartData.labels = Object.keys(dates);
+    chartData.data = map(dates, day => {
       return day.reduce((a, b) => {
         return filter(b) ? a += Math.abs(b.amount):  a;
       }, 0)
     })
-    return input;
+    return chartData;
+  }
+
+  generateMonthlyChart (transactions, filter = identity) {
+    const months = this.assignToMonth(transactions);
+    const year = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const amounts = year.map(month => {
+      months[month] = months[month] || [];
+      return months[month].reduce((a, b) => {
+        return filter(b) ? a += Math.abs(b.amount) : a;
+      }, 0);
+    });
+    const chartData = {
+      labels: year, 
+      datasets:[
+      {
+        label: 'Monthly Amounts',
+        data: [...amounts]
+      }]
+    }
+    return chartData;
   }
 
   renderChart(value, callback) {
     let transactions = this.getTransactionsFromTo();
-    let output = this.generateChartInput(transactions, callback)
-    output[0].unshift(value);
-    output[1].unshift('x')
+    let chartData = this.generateMonthlyChart(transactions, callback)
     this.setState({
-      chartData: output
-    }, ()=> {
-      console.log(this.state.chartData)
-      this.forceUpdate()
-      this.chart.unload();
-      this.chart.load({
-        data: {
-          colums: [['x',...this.state.chartData[0]], [value,...this.state.chartData[1]]]
-        }
-      })
+      chartData
     })
   }
 
-  lineChart () {
-    this.chart = c3.generate({
-        bindto:  '#chart',
-        data: {
-          x: 'x',
-          columns: [...this.state.chartData], 
-          type: 'spline'
-        },
-        axis: {
-          x: {
-            type: 'timeseries'
-          },
-        },
-      })
+  handleMonthlyClick(element) {
+
   }
 
   componentDidMount() {
     let { name } = this.props.summaryTransaction;
     let range = this.getTransactionsFromTo();
-    let transactions = this.generateChartInput(range, (transaction) => transaction.name === name);
-    transactions[0].unshift(name);
-    transactions[1].unshift('x')
-    this.setState({
-      chartData: transactions
-    }, () => this.lineChart())
+    let chartData = this.generateMonthlyChart(range, (transaction) => transaction.name === name);
+    console.log(chartData)
+    this.setState({chartData})
   }
 
   render() {
+      const options = {
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Chart'
+      },
+      tooltips: {
+        mode: 'label'
+      },
+      hover: {
+        mode: 'dataset'
+      },
+      scales: {
+        xAxes: [
+          {
+            display: true,
+            scaleLabel: {
+              show: true,
+              labelString: 'Month'
+            }
+          }
+        ],
+        yAxes: [
+          {
+            display: true,
+            scaleLabel: {
+              show: true,
+              labelString: 'Value'
+            },
+            ticks: {
+              suggestedMin: 0,
+              suggestedMax: 5000
+            }
+          }
+        ]
+      }
+    }
     return (
       <div>
-      <div style={{width: '600px', height: '500px'}} id="chart">
-      </div>
+        <Line data={this.state.chartData} options={options} width="600" height="500" getElementAtEvent={(element) => this.handleMonthlyClick(element)}/>
         <Select 
           placeholder="Select a category"
           options={this.props.categories.map(a => a[0])}
           onChange={({value}) => {
+            console.log(value)
             this.renderChart(value, (transaction) => {
               return transaction.category === value
             })
-            // let data = this.generateChartInput(this.props.transactions, (transaction) => {
-            //   return transaction.category === value;
-            // })
-            // console.log(data)
-            // this.chart
-            // .unload();
-            // this.chart
-            // .load({
-            //   columns: [
-            //     [value,...data[0]],
-            //     ['x', ...data[1]]
-            //   ]
-            // })
           }}
         />
       </div>
