f0805aa117e80a67148906254da2f95d55a2e761
GustafN <gustaf.brostedt@gmail.com>
2018-02-08 15:50:47 -0500

    helper

--- client/components/pages/transactions/Chart/TransactionsSummary.jsx
+++ client/components/pages/transactions/Chart/TransactionsSummary.jsx
@@ -5,6 +5,18 @@ import Layer from 'grommet/components/Layer';
 import { identity, map, unionBy } from 'lodash';
 import moment from 'moment'
 import TransactionChart from './SummaryChart.jsx'
+import {
+  generateChartDataObject,
+  assignToDate,
+  assignToMonth,
+  filterTransactionsByValue,
+  generateDailyData,
+  generateMonthlyData
+} from './chartHelpers.jsx'
+// const sampleGoals = {
+//   goal1: {car: 5000, date: '2018-02-24' }
+//   goal2: 3000
+// }
 
 class SummaryChartContainer extends React.Component {
   constructor() {
@@ -16,106 +28,108 @@ class SummaryChartContainer extends React.Component {
       month: '',
       filter: {},
       filteredTransactions: [],
+      accounts: [],
+      categories: [],
     }
-    this.handleChartClick = this.handleChartClick.bind(this)
-    this.generateMonthlyData = this.generateMonthlyData.bind(this);
-    this.generateDailyData = this.generateDailyData.bind(this);
-    this.renderChart = this.renderChart.bind(this);
-    this.toggleTotal = this.toggleTotal.bind(this);
+    // this.handleChartClick = this.handleChartClick.bind(this)
+    // this.generateMonthlyData = this.generateMonthlyData.bind(this);
+    // this.generateDailyData = this.generateDailyData.bind(this);
+    // this.renderChart = this.renderChart.bind(this);
+    // this.toggleTotal = this.toggleTotal.bind(this);
   }
 
-  // Formats data in preparation for chart
+  // // Formats data in preparation for chart
 
-  generateChartDataObject (labels, data) {
-    const chartData = {
-      labels: labels, 
-      datasets:[
-      {
-        label: 'Monthly Amounts',
-        data: [...data],
-        backgroundColor: "rgb(71, 255, 178)"
-      }]
-    }
-    return chartData;   
-  }
+  // generateChartDataObject (labels, data) {
+  //   const chartData = {
+  //     labels: labels, 
+  //     datasets:[
+  //     {
+  //       label: 'Monthly Amounts',
+  //       data: [...data],
+  //       backgroundColor: "rgb(71, 255, 178)"
+  //     }]
+  //   }
+  //   return chartData;   
+  // }
 
-  // Filters transactions according to filter object in state
-  // if transaction matches all properties of filter, it is returned
+  // // Filters transactions according to filter object in state
+  // // if transaction matches all properties of filter, it is returned
 
-  filterTransactionsByValue(callback) {
-    if (this.props.transactions) {
-      let transactions = this.props.transactions.filter(transaction => {
-        return Object.keys(this.state.filter).every((key) => {
-          if (key === 'account') {
-            return transaction[key][0].bank_name === this.state.filter[key];
-          }
-          return transaction[key] === this.state.filter[key]
-        })
-      })
-      console.log(transactions)
-      return transactions
-    }
-  }
+  // filterTransactionsByValue() {
+  //   if (this.props.transactions) {
+  //     let transactions = this.props.transactions.filter(transaction => {
+  //       return Object.keys(this.state.filter).every((key) => {
+  //         if (key === 'account') {
+  //           return transaction[key][0].bank_name === this.state.filter[key];
+  //         }
+  //         return transaction[key] === this.state.filter[key]
+  //       })
+  //     })
+  //     console.log(transactions)
+  //     return transactions
+  //   }
+  // }
 
-  // Groups annual transactions by date
+  // // Groups annual transactions by date
 
-  assignToDate (transactions) {
-    let dates = {};
-    transactions.forEach(transaction => {
-      let date = transaction.date;
-      dates[date] ?
-      dates[date].push(transaction) :
-      dates[date] = [transaction];
-    })
-    return dates;
-  }
+  // assignToDate (transactions) {
+  //   let dates = {};
+  //   transactions.forEach(transaction => {
+  //     let date = transaction.date;
+  //     dates[date] ?
+  //     dates[date].push(transaction) :
+  //     dates[date] = [transaction];
+  //   })
+  //   return dates;
+  // }
 
-    // Groups annual transactions by month
+  //   // Groups annual transactions by month
 
-  assignToMonth (transactions) {
-    let months = {};
-    transactions.forEach(transaction => {
-      let month = moment(transaction.date).format('MMMM')
-      months[month] ?
-      months[month].push(transaction) :
-      months[month] = [transaction];
-    })
-    return months;
-  }
+  // assignToMonth (transactions) {
+  //   let months = {};
+  //   transactions.forEach(transaction => {
+  //     let month = moment(transaction.date).format('MMMM')
+  //     months[month] ?
+  //     months[month].push(transaction) :
+  //     months[month] = [transaction];
+  //   })
+  //   return months;
+  // }
 
-  // Takes daily transactions and calculates total spend for the day for each date
-  // then sets state to rerender the chart
+  // // Takes daily transactions and calculates total spend for the day for each date
+  // // then sets state to rerender the chart
 
-  generateDailyData (transactions, month) {
-    const monthlyTransactions = this.assignToMonth(transactions)[month];
-    if (monthlyTransactions) {
-      const dailyTransactions = this.assignToDate(monthlyTransactions)
-      // for some reason map didn't like an object-like array...
-      const days = [...Object.keys(dailyTransactions)];
-      const amounts = days.map(day => {
-        return dailyTransactions[day].reduce((a, b) => {
-          return  a += Math.abs(b.amount);
-        }, 0)
-      })
-      return this.generateChartDataObject(days, amounts);
-    }
-    return this.generateChartDataObject([], [])
-  }
+  // generateDailyData (transactions, month) {
+  //   const monthlyTransactions = this.assignToMonth(transactions)[month];
+  //   if (monthlyTransactions) {
+  //     const dailyTransactions = this.assignToDate(monthlyTransactions)
+  //     // for some reason map didn't like an object-like array...
+  //     const days = [...Object.keys(dailyTransactions)];
+  //     const amounts = days.map(day => {
+  //       return dailyTransactions[day].reduce((a, b) => {
+  //         return  a += Math.abs(b.amount);
+  //       }, 0)
+  //     })
+  //     return this.generateChartDataObject(days, amounts);
+  //   }
+  //   return this.generateChartDataObject([], [])
+  // }
 
-  // Takes monthly transactions and calculates total spend for the month
-  // then sets state to rerender the chart
+  // // Takes monthly transactions and calculates total spend for the month
+  // // then sets state to rerender the chart
 
-  generateMonthlyData (transactions) {
-    const months = this.assignToMonth(transactions);
-    const year = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const amounts = year.map(month => {
-      months[month] = months[month] || [];
-      return months[month].reduce((a, b) => {
-        return a += Math.abs(b.amount)
-      }, 0);
-    });
-    return this.generateChartDataObject(year, amounts);
-  }
+  // generateMonthlyData (transactions) {
+  //   const months = this.assignToMonth(transactions);
+  //   const year = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  //   const amounts = year.map(month => {
+  //     months[month] = months[month] || [];
+  //     return months[month].reduce((a, b) => {
+  //       return a += Math.abs(b.amount)
+  //     }, 0);
+  //   });
+  //   return this.generateChartDataObject(year, amounts);
+  // }
 
   // Below checks whether or not a month has been selected
   // if true, the chart will render daily transactions for the selected month
@@ -136,23 +150,38 @@ class SummaryChartContainer extends React.Component {
     })
   }
 
+  // Enables users to add a total spending dataset to the chart
+  // in order to compare filtered data to total spend.
+
   toggleTotal () {
     let { chartData } = this.state, total;
-    console.log(total)
-    if (this.state.displayAnnual) {
-      total = this.generateMonthlyData(this.props.transactions);
-      total.datasets[0].label = 'Monthly Totals'
+
+    // checks state of the chart
+    if (!this.state.displayTotal) {
+
+      if (this.state.displayAnnual) {
+        total = this.generateMonthlyData(this.props.transactions);
+        total.datasets[0].label = 'Monthly Totals'
+      } else {
+        total = this.generateDailyData(this.props.transactions, this.state.month);
+        total.datasets[0].label = 'Daily Totals'
+      }
+
+      total.datasets[0].backgroundColor = "rgb(148, 0, 211)"
+      chartData.datasets.push(total.datasets[0]);
+      // if total is already displayed, remove it
     } else {
-      total = this.generateDailyData(this.props.transactions, this.state.month);
-      total.datasets[0].label = 'Daily Totals'
+      chartData.datasets.splice(1)
     }
-    total.datasets[0].backgroundColor = "rgb(148, 0, 211)"
-    chartData.datasets.push(total.datasets[0]);
-    this.setState({chartData})
+    this.setState({
+      chartData,
+      displayTotal: !this.state.displayTotal
+    })
   }
 
   renderChart() {
     const filteredTransactions = this.filterTransactionsByValue();
+    // check current state of the chart
     const chartData = this.state.displayAnnual ?
     this.generateMonthlyData(filteredTransactions) :
     this.generateDailyData(filteredTransactions, this.state.month);
@@ -165,6 +194,17 @@ class SummaryChartContainer extends React.Component {
 
   componentDidMount(){
     this.renderChart();
+  }
+
+  componentWillReceiveProps({categories, accounts}){
+    categories = categories.map(a => a[0]);
+    accounts = accounts.map(a => a.bank_name);
+    accounts.unshift('none');
+    categories.unshift('none');
+    this.setState({
+      categories,
+      accounts,
+    })
   }
 
   render() {
@@ -187,21 +227,25 @@ class SummaryChartContainer extends React.Component {
           onChange={() => this.toggleTotal()}/>
         <Select 
           placeHolder="Select a category"
-          options={this.props.categories.map(a => a[0])}
+          options={this.state.categories}
           value={this.state.filter.category}
           onChange={({value}) => {
             let { filter } = this.state;
+            value === 'none' ?
+            delete filter.category :
             filter.category = value;
             this.setState({filter}, () => this.renderChart())
           }}
         />
         <Select 
           placeHolder="Select an account"
-          options={this.props.accounts.map(a => a.bank_name)}
+          options={this.state.accounts}
           value={this.state.filter.account}
           onChange={({value}) => {
             let { filter } = this.state;
-            filter.account = value;
+            value === 'none' ?
+            delete filter.category :
+            filter.category = value;
             this.setState({filter}, () => this.renderChart())
           }}
         />
