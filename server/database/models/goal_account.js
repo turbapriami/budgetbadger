6e2de821962c9a1cec727e56c56cf085d712f63c
Your Name <andrewbl@icloud.com>
2018-02-08 19:59:24 -0500

    goals form progressing

--- server/database/models/goal_account.js
+++ server/database/models/goal_account.js
@@ -4,6 +4,9 @@ const GoalCategory = db.Model.extend({
   tableName: 'goal_accounts',  
   goal_id: () => {
     return this.belongsTo(Goal, 'id');
+  },
+  account_id: () => {
+    return this.belongsTo(Account, 'id');
   }
 })
 
