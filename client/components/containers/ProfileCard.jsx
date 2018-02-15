d6867be7d02392a0d5943d3e3b5c54c2662564da
afriedman1991@gmail.com <afriedman1991@gmail.com>
2018-02-15 17:48:25 -0500

    Can submit changes by pressing enter when editing user profile

--- client/components/containers/ProfileCard.jsx
+++ client/components/containers/ProfileCard.jsx
@@ -12,10 +12,18 @@ const ProfileCard = (props) => {
                 {props.userInfo.editName === false ? <h1 onClick={props.editName} >{props.userInfo.first_name} {props.userInfo.last_name}</h1> :     
                 <h3><b onClick={props.handleSubmit} >Name: </b>
                   <li style={{ listStyleType: "none" }}>
-                    <b>First Name: </b><TextInput onDOMChange={(e) => props.handleForm(e)} name="first_name" defaultValue={props.userInfo.first_name}/>
+                    <b>First Name: </b><TextInput onKeyPress={(e) => {
+                      if (e.key === 'Enter') {
+                        props.handleSubmit();
+                      }
+                    }} onDOMChange={(e) => props.handleForm(e)} name="first_name" defaultValue={props.userInfo.first_name}/>
                   </li>
                   <li style={{ listStyleType: "none" }}>
-                    <b>Last Name: </b><TextInput onDOMChange={(e) => props.handleForm(e)} name="last_name" defaultValue={props.userInfo.last_name}/>
+                    <b>Last Name: </b><TextInput onKeyPress={(e) => {
+                      if (e.key === 'Enter') {
+                        props.handleSubmit();
+                      }
+                    }} onDOMChange={(e) => props.handleForm(e)} name="last_name" defaultValue={props.userInfo.last_name}/>
                   </li>
                 </h3>}
               </Paragraph>
@@ -24,13 +32,25 @@ const ProfileCard = (props) => {
               { props.userInfo.editAddress === false ? <h3><b onClick={props.editAddress} >Address: </b>{props.userInfo.street}, {props.userInfo.state}, {props.userInfo.zip_code}</h3> : 
               <h3><b onClick={props.handleSubmit} >Address: </b>
                 <li style={{ listStyleType: "none" }}>
-                  <b>Street </b><TextInput onDOMChange={(e) => props.handleForm(e)} name="street" defaultValue={props.userInfo.street}/>
+                  <b>Street </b><TextInput onKeyPress={(e) => {
+                      if (e.key === 'Enter') {
+                        props.handleSubmit();
+                      }
+                    }} onDOMChange={(e) => props.handleForm(e)} name="street" defaultValue={props.userInfo.street}/>
                 </li>
                 <li style={{ listStyleType: "none" }}>
-                  <b>State </b><TextInput onDOMChange={(e) => props.handleForm(e)} name="state" defaultValue={props.userInfo.state}/>
+                  <b>State </b><TextInput onKeyPress={(e) => {
+                      if (e.key === 'Enter') {
+                        props.handleSubmit();
+                      }
+                    }} onDOMChange={(e) => props.handleForm(e)} name="state" defaultValue={props.userInfo.state}/>
                 </li>
                 <li style={{ listStyleType: "none" }}>
-                  <b>ZIP Code </b><TextInput onDOMChange={(e) => props.handleForm(e)} name="zip_code" defaultValue={props.userInfo.zip_code}/>
+                  <b>ZIP Code </b><TextInput onKeyPress={(e) => {
+                      if (e.key === 'Enter') {
+                        props.handleSubmit();
+                      }
+                    }} onDOMChange={(e) => props.handleForm(e)} name="zip_code" defaultValue={props.userInfo.zip_code}/>
                 </li>
               </h3> }
             </Paragraph>
@@ -39,7 +59,11 @@ const ProfileCard = (props) => {
               { props.userInfo.editEmail === false ? <h3><b onClick={props.editEmail} >Email: </b>{props.userInfo.email}</h3>: 
               <h3><b onClick={props.handleSubmit} >Email: </b>
                 <li style={{ listStyleType: "none" }}>
-                  <TextInput onDOMChange={(e) => props.handleForm(e)} name="email" defaultValue={props.userInfo.email}/>
+                  <TextInput onKeyPress={(e) => {
+                      if (e.key === 'Enter') {
+                        props.handleSubmit();
+                      }
+                    }} onDOMChange={(e) => props.handleForm(e)} name="email" defaultValue={props.userInfo.email}/>
                 </li>
               </h3>
               }
@@ -49,7 +73,11 @@ const ProfileCard = (props) => {
               { props.userInfo.editPhone === false ? <h3><b onClick={props.editPhone} >Phone: </b>{props.userInfo.phone}</h3> :
               <h3><b onClick={props.handleSubmit} >Phone: </b>
               <li style={{ listStyleType: "none" }} >
-                <TextInput size={{ height: "small", width: "large"}} onDOMChange={(e) => props.handleForm(e)} name="phone" defaultValue={props.userInfo.phone}/>
+                <TextInput size={{ height: "small", width: "large"}} onKeyPress={(e) => {
+                      if (e.key === 'Enter') {
+                        props.handleSubmit();
+                      }
+                    }} onDOMChange={(e) => props.handleForm(e)} name="phone" defaultValue={props.userInfo.phone}/>
               </li>
               </h3> }
             </Paragraph>
